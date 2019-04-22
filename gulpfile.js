594ba0d173957f6bb8bb59d3e12249fb642cf248
Oshchenkov <oshchenkovv@gmail.com>
2019-04-22 14:27:32 +0300

    Start settings

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+var gulpVersion   = '4'; // Gulp version: 3 or 4
+
+var     gulp          = require('gulp'),
+		gutil         = require('gulp-util' ),
+		sass          = require('gulp-sass'),
+		browserSync   = require('browser-sync'),
+		concat        = require('gulp-concat'),
+		uglify        = require('gulp-uglify'),
+		cleancss      = require('gulp-clean-css'),
+		rename        = require('gulp-rename'),
+		autoprefixer  = require('gulp-autoprefixer'),
+		notify        = require('gulp-notify');
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// open: false,
+		// online: false, // Work Offline Without Internet Connection
+		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+	})
+});
+
+gulp.task('styles', function() {
+	return gulp.src('app/sass/**/*.+(scss|sass)')
+	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(rename({ 
+		basename: 'style',
+	}))
+	.pipe(gulp.dest('app'))
+	.pipe(browserSync.stream())
+});
+gulp.task('stylesMin', function() {
+	return gulp.src('app/sass/**/*.+(scss|sass)')
+	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+	.pipe(rename({ 
+		basename: 'style',
+		suffix: '.min',
+	}))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream())
+});
+
+gulp.task('scripts', function() {
+	return gulp.src([
+		//'app/libs/jquery/dist/jquery.min.js',
+		'app/js/common.js', // Always at the end
+		])
+	.pipe(concat('scripts.min.js'))
+	// .pipe(uglify()) // Mifify js (opt.)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('code', function() {
+	return gulp.src('app/*.html')
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+
+if (gulpVersion == 3) {
+	gulp.task('watch', ['styles', 'stylesMin', 'scripts', 'browser-sync'], function() {
+		gulp.watch('app/sass/**/*.+(scss|sass)', ['styles','stylesMin']);
+		gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['scripts']);
+		gulp.watch('app/*.html', ['code'])
+	});
+	gulp.task('default', ['watch']);
+}
+
+if (gulpVersion == 4) {
+	gulp.task('watch', function() {
+		gulp.watch('app/sass/**/*.+(scss|sass)', gulp.parallel(['styles','stylesMin']));
+		gulp.watch(['libs/**/*.js', 'app/js/common.js'], gulp.parallel('scripts'));
+		gulp.watch('app/*.html', gulp.parallel('code'))
+	});
+	gulp.task('default', gulp.parallel('styles','stylesMin', 'scripts', 'browser-sync', 'watch'));
+}
